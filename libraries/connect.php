1d1c8c22a523db20b1d5a3eb362bc14120ac7e5e
Masryan Siregar <smilingrey@gmail.com>
2016-01-12 15:00:59 +0700

    koneksi server

--- libraries/connect.php
+++ libraries/connect.php
@@ -1,4 +1,7 @@
 <?php
-	$host = mysql_connect("localhost", "root", "");
+	//$host = mysql_connect("localhost", "root", "");
+	//mysql_select_db("db_alia", $host);	
+	
+	$host = mysql_connect("localhost", "root", "root1234");
 	mysql_select_db("db_alia", $host);	
 ?>
